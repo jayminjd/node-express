5ef4f58e3cb70ecc40c5feade7bf9710676e5554
jayminjd <jaymindarji999@gmail.com>
2021-07-26 14:30:58 +0530

    react+firebase-crud

---
+++ Desktop/MERN/ReactWithFirebase/react-firebase-crud/src/services/tutorial.service.js
@@ -0,0 +1,23 @@
+import firebaseStore from "../firebase";
+
+const db = firebaseStore.collection("/tutorials");
+
+class TutorialDataService {
+  getAll() {
+    return db;
+  }
+
+  create(tutorial) {
+    return db.add(tutorial);
+  }
+
+  update(id, value) {
+    return db.doc(id).update(value);
+  }
+
+  delete(id) {
+    return db.doc(id).delete();
+  }
+}
+
+export default new TutorialDataService();
