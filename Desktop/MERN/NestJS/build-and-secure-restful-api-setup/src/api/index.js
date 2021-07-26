ff89343d905068bb0b25094e2f7c8d694c4419e8
jayminjd <jaymindarji999@gmail.com>
2021-07-26 15:51:32 +0530

    git

---
+++ Desktop/MERN/NestJS/build-and-secure-restful-api-setup/src/api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { songRouter } from './resources/songs';
+
+export const restRouter=express.Router();
+
+restRouter.use('/songs',songRouter) 
+
+
---
+++ Desktop/MERN/NestJS/build-and-secure-restful-api-setup/src/api/resources/songs/index.js
@@ -0,0 +1,3 @@
+export {songRouter} from './song.router'
+
+
